c998e4e53073293aaeedc23df0cfb3de84b0c68b
0707Panda <90808764+0707Panda@users.noreply.github.com>
2022-07-15 09:25:26 -0400

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,17 @@
+function Logout(){
+    window.location="index.html";
+}
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB4ZHeBlV2QQ-nwMvvFNfE-wqzscEB3Le4",
+    authDomain: "kwitterhwproject.firebaseapp.com",
+    projectId: "kwitterhwproject",
+    storageBucket: "kwitterhwproject.appspot.com",
+    messagingSenderId: "946776388089",
+    appId: "1:946776388089:web:8219813e7c2e216b050d28",
+    measurementId: "G-4WF8CEL4BV"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
